dc534bb1ee61404ac7a099bacfa9ee4e328c160b
harry413 <sachinpatidar413@gmail.com>
2023-05-25 10:40:25 +0530

    changes in links

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
-        let top =window.scrollY;
+        let top = window.scrollY;
         let offset = sec.offsetTop - 150;
-        let height =sec.offsetHeight;
+        let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height){
